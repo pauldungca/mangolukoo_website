447510750ae7abdf00647645ad351c52ab831a21
pauldungca <johnpauldungca0908@gmail.com>
2026-01-30 15:47:38 +0800

    Home UI Done!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
+import Branches from "./pages/Branches";
+import Menu from "./pages/Menu";
+import ContactUs from "./pages/ContactUs";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about-us" element={<AboutUs />} />
+          <Route path="branches" element={<Branches />} />
+          <Route path="menu" element={<Menu />} />
+          <Route path="contact-us" element={<ContactUs />} />
         </Route>
       </Routes>
     </BrowserRouter>
